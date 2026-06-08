5508d40c2d06ff6e402b55680d0e12a04e9eae10
Soham Chowdhury <soham.chowdhury.2026@gmail.com>
2026-06-08 11:16:26 +0530

    Add files via upload

---
+++ Simion say game/script.js
@@ -0,0 +1,82 @@
+let gameSeq = [];
+let userSeq = [];
+let btns = ["yellow","red","purple","green"];
+
+let started = false;
+let level = 0;
+
+let h3 = document.querySelector("h3");
+
+document.addEventListener("keypress",function(){
+    if(started == false){
+        console.log("game started");
+        started = true;
+
+        levelUp();
+    }
+});
+
+function btnFlash(btn) {
+    btn.classList.add("flash")
+    setTimeout(function() {
+        btn.classList.remove("flash");
+    },250);
+}
+
+function userFlash(btn) {
+    btn.classList.add("userflash")
+    setTimeout(function() {
+        btn.classList.remove("userflash");
+    },250);
+}
+
+function checkAns(idx) {
+    if(userSeq[idx] == gameSeq[idx]) {
+        if(userSeq.length == gameSeq.length){ 
+            setTimeout(levelUp,1000) 
+        }
+    } else {
+        h3.innerHTML = `Game Over! Your score was <b>${level}</b> <br>Press any key to start`;
+        document.querySelector("body").style.backgroundColor = "red";
+        setTimeout(function(){
+            document.querySelector("body").style.backgroundColor = "white";
+        },150);
+        reset();
+    }
+}
+
+function levelUp() {
+    userSeq = [];
+    level++;
+    h3.innerText = `Level ${level}`;
+
+    //random btn choose
+    let randIdx =Math.floor(Math.random()*3);
+    let randcolor = btns[randIdx];
+    let randbtn = document.querySelector(`.${randcolor}`);
+
+    gameSeq.push(randcolor);
+    btnFlash(randbtn);
+}
+
+function btnPress() {
+    let btn = this;
+    userFlash(btn);
+
+    userColor = btn.getAttribute("id");
+    userSeq.push(userColor);
+
+    checkAns(userSeq.length-1)
+}
+
+let allBtns = document.querySelectorAll(".btn");
+for(btn of allBtns) {
+    btn.addEventListener("click",btnPress)
+}
+
+function reset() {
+    started = false;
+    gameSeq = [];
+    userSeq = [];
+    level = 0;  
+}

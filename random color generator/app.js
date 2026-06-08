2be4b27a73cab39794a4a0f9cdeef71ca6c95f4b
Soham Chowdhury <soham.chowdhury.2026@gmail.com>
2026-06-08 11:16:04 +0530

    Add files via upload

---
+++ random color generator/app.js
@@ -0,0 +1,23 @@
+let btn = document.querySelector("button");
+
+btn.addEventListener("click", function(){
+    let h3 = document.querySelector("h3");
+    let randomColor = getRandomColor();
+    h3.innerText = randomColor;
+    h3.style.color = randomColor;
+    console.log("color updated");
+
+    let div = document.querySelector("div");
+    div.style.background = randomColor;
+
+});
+
+function getRandomColor() {
+    let red = Math.floor(Math.random()*255);
+    let green = Math.floor(Math.random()*255);
+    let blue = Math.floor(Math.random()*255);
+
+    let color = `rgb(${red}, ${green}, ${blue})`;
+    return color;
+}
+
